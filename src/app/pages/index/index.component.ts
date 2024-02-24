d828799d24945af789e8fbbad52cccfe18c3ec98
Javaluca <g.pellegrini89@gmail.com>
2024-02-24 22:56:18 +0100

    Basic ui improvements

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
+
+  public url: string = '';
+
+  constructor(private router: Router) {
+
+  }
+
+  onEnter(event: Event): void {
+    console.log(event);
+    if (!this.url) {
+      return;
+    }
+    this.router.navigate(['browse'])
+  }
 
 }
