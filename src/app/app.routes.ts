91f24841b1c243c818df820d3c224c123a9ac5ff
Javaluca <g.pellegrini89@gmail.com>
2024-02-24 23:03:35 +0100

    Added page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BrowserComponent } from './pages/browser/browser.component';
 import { IndexComponent } from './pages/index/index.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/index', pathMatch: 'full' },
     {path: 'index', component: IndexComponent },
-    {path: 'browse', component: BrowserComponent }
+    {path: 'browse', component: BrowserComponent },
+    {path: '**', component: PageNotFoundComponent },
 ];
